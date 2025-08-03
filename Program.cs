503caff893ce6011e7e34e59a2c66645e0e60f4e
zenin5479 <zenin5479@gmail.com>
2025-08-03 22:46:54 +0300

    Update 0.1 Обновлен метод. Проведен рефакторинг

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace Homework_5._4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World");
         }
     }
 }

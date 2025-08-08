fcdfaa0f1335675cd27244df5a133c34e8ef8a9e
zenin5479 <zenin5479@gmail.com>
2025-08-08 13:22:16 +0300

    Update 8.5 Обновлен метод. Проведен рефакторинг

--- Program.cs
+++ Program.cs
@@ -35,7 +35,7 @@ namespace Homework_5._4
             double[,] input2DArray = MethodsFor2DArray.InputMatrixDouble(source2DArray, n, name2DArray);
             int negative1DArray = MethodsFor1DArray.SearchingNegativeDouble(input1DArray, name1DArray);
             int negative2DArray = MethodsFor2DArray.SearchingNegativeDouble(input2DArray, name2DArray);
-            //double[,] sortArray = VariousMethods.BubbleSortArray(inputArray, sumRow);
+            
             //string pathFileInput = Path.GetFullPath(nameFileInput);
             //File.Create(pathFileInput).Close();
             //string[] arrayLines = VariousMethods.OutputArrayString(sortArray);

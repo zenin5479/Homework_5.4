85f89298f8d1a2a2ff8034c9f6f6dc613358725c
zenin5479 <zenin5479@gmail.com>
2025-08-10 17:44:02 +0300

    Update 13.2 Обновлен метод. Проведен рефакторинг

--- Program.cs
+++ Program.cs
@@ -47,8 +47,7 @@ namespace Homework_5._4
             }
             else
             {
-               Console.WriteLine("В одномерном массиве {0} больше отрицательных элементов чем в двумерном массиве {1}",
-                  name1DArray, name2DArray);
+               Console.WriteLine("В одномерном массиве {0} больше отрицательных элементов чем в двумерном массиве {1}", name1DArray, name2DArray);
                double[,] sortArray = SwapLastLine(input2DArray);
                string pathFileInput = Path.GetFullPath(nameFileInput);
                File.Create(pathFileInput).Close();
